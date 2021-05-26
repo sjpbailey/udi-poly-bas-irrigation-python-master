4bb6c296f81bef136476b675dde992b51323e1da
sjpbailey <sjpbailey1961@gmail.com>
2021-05-26 12:02:19 -0700

    Commits

--- nodes/BaspiNodeOne.py
+++ nodes/BaspiNodeOne.py
@@ -21,176 +21,98 @@ class BaspiNodeOne(polyinterface.Node):
         if self.ipaddress is not None:
             self.bc = Device(self.ipaddress)
                   
-        ### BASpi One ###
-        if self.bc.ePlatform == Platform.BASC_NONE:
-            LOGGER.info('Unable to connect')
-            LOGGER.info('ipaddress')
-        if self.bc.ePlatform == Platform.BASC_PI:
-            LOGGER.info('Connected to BASpi6U6R Module One')
-        if self.bc.ePlatform == Platform.BASC_PO:
-            LOGGER.info('Connected to BASpi6U6R Module One')
-        if self.bc.ePlatform == Platform.BASC_ED:
-            LOGGER.info('Connected to BASpi-Edge Module One')    
+        ### Do we have a BASpi or an Edge Device ###
+        if self.bc.ePlatform == Platform.BASC_PI or self.bc.ePlatform == Platform.BASC_PO: ### if a BASpi-6u6r Device is found
+            LOGGER.info('connected to BASpi-6U6R')
+        elif self.bc.ePlatform == Platform.BASC_ED: ### if a BASpi-Edge Device is found
+            LOGGER.info('connected to BASpi-Edge')
             self.setDriver('ST', 1)    
-
-        if self.bc.ePlatform == Platform.BASC_ED:
-                LOGGER.info('Universal inputs in this BASpi-EDGE' + '\t' + str(self.bc.uiQty))
-                LOGGER.info('Binary outputs in this BASpi-EDGE' + '\t' + str(self.bc.boQty))
-        if self.bc.ePlatform == Platform.BASC_PI:
-                LOGGER.info('Universal inputs in this BASpi-6U6R' + '\t' + str(self.bc.uiQty))
-                LOGGER.info('Binary outputs in this BASpi-6U6R' + '\t' + str(self.bc.boQty))
-        if self.bc.ePlatform == Platform.BASC_PO:
-                LOGGER.info('Universal inputs in this BASpi-6U6R' + '\t' + str(self.bc.uiQty))
-                LOGGER.info('Binary outputs in this BASpi-6U6R' + '\t' + str(self.bc.boQty))        
+        elif self.bc.ePlatform == Platform.BASC_NONE: ### if there is NO Device found
+            LOGGER.info('Unable to connect to Device')
+            LOGGER.info('ipaddress')
+        else:
+            pass
+        
+        # How many nodes or points does the device have
+        LOGGER.info('\t' + str(self.bc.uiQty) + ' Universal inputs in this Device')
+        LOGGER.info('\t' + str(self.bc.boQty) + ' Binary outputs in this Device')
+        LOGGER.info('\t' + str(self.bc.vtQty) + ' Virtual points In This Device')        
         
         # Input/Output Status
-        LOGGER.info('UI 1' + '\t' + str(self.bc.universalInput(1)))
-        LOGGER.info('UI 2' + '\t' + str(self.bc.universalInput(2)))
-        LOGGER.info('UI 3' + '\t' + str(self.bc.universalInput(3)))
-        LOGGER.info('UI 4' + '\t' + str(self.bc.universalInput(4)))
-        LOGGER.info('UI 5' + '\t' + str(self.bc.universalInput(5)))
-        LOGGER.info('UI 6' + '\t' + str(self.bc.universalInput(6)))
-        LOGGER.info('BO 1' + '\t' + str(self.bc.binaryOutput(1)))
-        LOGGER.info('BO 2' + '\t' + str(self.bc.binaryOutput(2)))
-        LOGGER.info('BO 3' + '\t' + str(self.bc.binaryOutput(3)))
-        LOGGER.info('BO 4' + '\t' + str(self.bc.binaryOutput(4)))
-        LOGGER.info('BO 5' + '\t' + str(self.bc.binaryOutput(5)))
-        LOGGER.info('BO 6' + '\t' + str(self.bc.binaryOutput(6)))
-
-        ### Universal Inputs ###
-        input_one = self.bc.universalInput(1)
-        input_two = self.bc.universalInput(2)
-        input_thr = self.bc.universalInput(3)
-        input_for = self.bc.universalInput(4)
-        input_fiv = self.bc.universalInput(5)
-        input_six = self.bc.universalInput(6)
-                        
-        ### Universal Inputs ###            
-        self.setDriver('GV0', input_one, force=True)
-        self.setDriver('GV1', input_two, force=True)
-        self.setDriver('GV2', input_thr, force=True)
-        self.setDriver('GV3', input_for, force=True)
-        self.setDriver('GV4', input_fiv, force=True)
-        #self.setDriver('GV5', input_six, force=True)   
+        LOGGER.info('Inputs')
+        for i in range(1,7):
+               LOGGER.info(str(self.bc.universalInput(i)))
+        LOGGER.info('Outputs')
+        for i in range(1,7):
+               LOGGER.info(str(self.bc.binaryOutput(i)))
         
-        # Binary/Digital Outputs
-        output_one = (self.bc.binaryOutput(1))
-        output_two = (self.bc.binaryOutput(2))
-        output_tre = (self.bc.binaryOutput(3))
-        output_for = (self.bc.binaryOutput(4))
-        output_fiv = (self.bc.binaryOutput(5))
-        output_six = (self.bc.binaryOutput(6))
-                       
-        # Binary/Digital Outputs
-        self.setDriver('GV6', output_one, force=True)
-        self.setDriver('GV7', output_two, force=True)
-        self.setDriver('GV8', output_tre, force=True)
-        self.setDriver('GV9', output_for, force=True)
-        self.setDriver('GV10', output_fiv, force=True)
-        self.setDriver('GV11', output_six, force=True)
-
         # Input 6 Conversion
+        input_six = self.bc.universalInput(6)
         if input_six is not None:
             sumss_count = int(float(input_six))
             self.setDriver('GV5', int(sumss_count), force=True)
         if input_six is not None:
-            sumss_count = int(float(self.bc.universalInput(6)))
-         
-    # OOP Control Commands
-    # Zone 1
-    def cmdOn1(self, command):
-        if self.bc.binaryOutput(1) != 1:
-            self.bc.binaryOutput(1, 1)
-            self.setDriver("GV6", 1) 
-            LOGGER.info('Zone 1 On')
-
-    def cmdOff1(self, command):
-        if self.bc.binaryOutput(1) != 0:
-            self.bc.binaryOutput(1, 0)
-            self.setDriver("GV6", 0) 
-            LOGGER.info('Zone 1 Off')
-
-    # Zone 2
-    def cmdOn2(self, command):
-        if self.bc.binaryOutput(2) != 1:
-            self.bc.binaryOutput(2, 1)
-            self.setDriver("GV7", 1) 
-            LOGGER.info('Zone 2 On')
-            
-    def cmdOff2(self, command):
-        if self.bc.binaryOutput(2) != 0:
-            self.bc.binaryOutput(2, 0)
-            self.setDriver("GV7", 0) 
-            LOGGER.info('Zone 2 Off')
-   
-    # Zone 3
-    def cmdOn3(self, command):
-        if self.bc.binaryOutput(3) != 1:
-            self.bc.binaryOutput(3, 1)
-            self.setDriver("GV8", 1) 
-            LOGGER.info('Zone 3 On')
-
-    def cmdOff3(self, command):
-        if self.bc.binaryOutput(3) != 0:
-            self.bc.binaryOutput(3, 0)
-            self.setDriver("GV8", 0) 
-            LOGGER.info('Zone 3 Off')
-
-    # Zone 4
-    def cmdOn4(self, command):
-        if self.bc.binaryOutput(4) != 1:
-            self.bc.binaryOutput(4, 1)
-            self.setDriver("GV9", 1)
-            LOGGER.info('Zone 4 On')
-           
-    def cmdOff4(self, command):
-        if self.bc.binaryOutput(4) != 0:
-            self.bc.binaryOutput(4, 0)
-            self.setDriver("GV9", 0) 
-            LOGGER.info('Zone 4 Off')
-   
-    # Zone 5
-    def cmdOn5(self, command):
-        if self.bc.binaryOutput(5) != 1:
-            self.bc.binaryOutput(5, 1)
-            self.setDriver("GV10", 1) 
-            LOGGER.info('Zone 5 On')
-            
-    def cmdOff5(self, command):
-        if self.bc.binaryOutput(5) != 0:
-            self.bc.binaryOutput(5, 0)
-            self.setDriver("GV10", 0) 
-            LOGGER.info('Zone 5 Off')
-    
-    # Zone 6
-    def cmdOn6(self, command):
-        if self.bc.binaryOutput(6) != 1:
-            self.bc.binaryOutput(6, 1)
-            self.setDriver("GV11", 1) 
-            LOGGER.info('Zone 6 On')
-            
-    def cmdOff6(self, command):
-        if self.bc.binaryOutput(6) != 0:
-            self.bc.binaryOutput(6, 0)
-            self.setDriver("GV11", 0) 
-            LOGGER.info('Zone 6 Off')
-
-    def shortPoll(self):
-        LOGGER.debug('shortPoll')
+            sumss_count = int(float(self.bc.universalInput(6))) 
+       
+        ### Universal Inputs ###
+        self.setInputDriver('GV0', 1)
+        self.setInputDriver('GV1', 2)
+        self.setInputDriver('GV2', 3)
+        self.setInputDriver('GV3', 4)
+        self.setInputDriver('GV4', 5)
         
-    def longPoll(self):
-        LOGGER.debug('longPoll')
+    def setInputDriver(self, driver, input):
+        input_val = self.bc.universalInput(input)
+     
+        # Binary/Digital Outputs
+        self.setOutputDriver('GV6', 1)
+        self.setOutputDriver('GV7', 2)
+        self.setOutputDriver('GV8', 3)
+        self.setOutputDriver('GV9', 4)
+        self.setOutputDriver('GV10', 5)
+        self.setOutputDriver('GV11', 6)
+        
+    def setOutputDriver(self, driver, input):
+        output_val = self.bc.binaryOutput(input)
+       
 
-    def cmd_ping(self,command):
-        LOGGER.debug("cmd_ping:")
-        r = self.http.request('GET',"google.com")
-        LOGGER.debug("cmd_ping: r={}".format(r))
+    # Dict for 6 output ON OFF function     
+        self.mapping = {
+            'BON1' : {'output':'GV6', 'index': (1)}, 
+            'BOF1' : {'output':'GV6', 'index': (1)}, 
+            'BON2' : {'output':'GV7', 'index': (2)}, 
+            'BOF2' : {'output':'GV7', 'index': (2)}, 
+            'BON3' : {'output':'GV8', 'index': (3)}, 
+            'BOF3' : {'output':'GV8', 'index': (3)},
+            'BON4' : {'output':'GV9', 'index': (4)}, 
+            'BOF4' : {'output':'GV9', 'index': (4)}, 
+            'BON5' : {'output':'GV10', 'index': (5)}, 
+            'BOF5' : {'output':'GV10', 'index': (5)}, 
+            'BON6' : {'output':'GV11', 'index': (6)}, 
+            'BOF6' : {'output':'GV11', 'index': (6)},
+            } 
+        
+    # ON OFF Control Commands
+    # All Zones
+    def cmdOn(self, command):
+        output = self.mapping[command['cmd']]['output']
+        index = self.mapping[command['cmd']]['index']
+        if self.bc.binaryOutput(index) != 1:
+            self.bc.binaryOutput(index, 1)
+            self.setDriver(output, 1) 
+            LOGGER.info('Zone {} On'.format(index))
+
+    def cmdOff(self, command):
+        output = self.mapping[command['cmd']]['output']
+        index = self.mapping[command['cmd']]['index']
+        if self.bc.binaryOutput(index) != 0:
+            self.bc.binaryOutput(index, 0)
+            self.setDriver(output, 0)
+            LOGGER.info('Zone {} Off'.format(index))
 
     def query(self,command=None):
         self.reportDrivers()
-
-    "Hints See: https://github.com/UniversalDevicesInc/hints"
-    hint = [1,2,3,4]
+    
     drivers = [
         {'driver': 'ST', 'value': 0, 'uom': 2},
         {'driver': 'GV0', 'value': 1, 'uom': 17},
@@ -213,18 +135,18 @@ class BaspiNodeOne(polyinterface.Node):
     the ISY what fields and commands this node has.
     """
     commands = {
-                    'BON1': cmdOn1,
-                    'BOF1': cmdOff1,
-                    'BON2': cmdOn2,
-                    'BOF2': cmdOff2,
-                    'BON3': cmdOn3,
-                    'BOF3': cmdOff3,
-                    'BON4': cmdOn4,
-                    'BOF4': cmdOff4,
-                    'BON5': cmdOn5,
-                    'BOF5': cmdOff5,
-                    'BON6': cmdOn6,
-                    'BOF6': cmdOff6,
+                    'BON1': cmdOn,
+                    'BOF1': cmdOff,
+                    'BON2': cmdOn,
+                    'BOF2': cmdOff,
+                    'BON3': cmdOn,
+                    'BOF3': cmdOff,
+                    'BON4': cmdOn,
+                    'BOF4': cmdOff,
+                    'BON5': cmdOn,
+                    'BOF5': cmdOff,
+                    'BON6': cmdOn,
+                    'BOF6': cmdOff,
                     'QUERY': query,
                 }
     
